0f2b275c32b70b17f3710ebfa9cc8f9ddae79c9f
jbmitchell7 <jbmitchell747@gmail.com>
2021-10-27 09:58:04 -0500

    added tests for first scenario

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import './App.css';
+import EventList from '../EventList';
 
 function App() {
   return (
     <div className="App">
-
+      <EventList />
     </div>
   );
 }
