3b338164f541cadd31adbb89e93cbfca8373c4b6
jbmitchell7 <jbmitchell747@gmail.com>
2021-11-04 15:06:53 -0500

    finished acceptance tests for tasks of the exercise

--- src/features/showHideDetails.test.js
+++ src/features/showHideDetails.test.js
@@ -2,52 +2,58 @@ import { loadFeature, defineFeature } from 'jest-cucumber';
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import App from '../App';
+import Event from '../components/Event/Event';
 import { mockData } from '../mock-data';
-import { extractLocations } from '../api';
 
 const feature = loadFeature('./src/features/showHideDetails.feature');
 
 defineFeature(feature, test => {
 
     test('An event element is collapsed by default', ({ given, when, then }) => {
-        given('the user is viewing all events', () => {
+        given('the user is viewing all events', () => { });
 
-        });
-
+        let AppWrapper;
         when('the user has not opened any event', () => {
-
+            AppWrapper = mount(<App />);
         });
 
         then('all events should be collapsed', () => {
-
+            AppWrapper.update();
+            expect(AppWrapper.find('.event-details').hostNodes()).toHaveLength(0);
         });
     });
 
     test('User can expand an event to see its details', ({ given, when, then }) => {
+        let EventWrapper;
         given('all events are collapsed', () => {
-
+            EventWrapper = shallow(<Event event={mockData[0]} />);
         });
 
         when('the user clicks to see a single events details', () => {
-
+            EventWrapper.find('#toggle-details').simulate('click');
         });
 
         then('the event\'s details should become visible', () => {
-
+            EventWrapper.update();
+            expect(EventWrapper.find('.event-details')).toHaveLength(1);
         });
     });
 
     test('User can collapse an event to hide details', ({ given, when, then }) => {
+        let EventWrapper;
         given('the user has expanded an event to view the details', () => {
-
+            EventWrapper = shallow(<Event event={mockData[0]} />);
+            EventWrapper.find('#toggle-details').simulate('click');
         });
 
         when('the user clicks to collapse the details', () => {
-
+            EventWrapper.update();
+            EventWrapper.find('#toggle-details').simulate('click');
         });
 
         then('the details should be collapsed', () => {
-
+            EventWrapper.update();
+            expect(EventWrapper.find('.event-details')).toHaveLength(0);
         });
     });
 
--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -1,9 +1,7 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import App from '../App';
-import { mockData } from '../mock-data';
-import { extractLocations } from '../api';
 
 const feature = loadFeature('./src/features/specifyNumberOfEvents.feature');
 
@@ -12,27 +10,29 @@ defineFeature(feature, test => {
         given('the user is viewing events', () => {
 
         });
-
+        let AppWrapper
         when('the user has not selected a number of events to view', () => {
-
+            AppWrapper = mount(<App />);
         });
 
         then('32 events should be displayed', () => {
-
+            expect(AppWrapper.state('numberOfEvents')).toBe(32);
         });
     });
 
     test('User can change the number of events they want to see', ({ given, when, then }) => {
+        let AppWrapper;
         given('the user is viewing events', () => {
-
+            AppWrapper = mount(<App />);
         });
 
         when('the user changes the number of events to view', () => {
-
+            AppWrapper.find('.form-range').simulate('change', { target: { value: 10 } });
         });
 
         then('the number of events in view should change to the selected value', () => {
-
+            AppWrapper.update();
+            expect(AppWrapper.state('numberOfEvents')).toBe(10);
         });
     });
 });
