1a4301ecfa2314e3d637876888dfacbd25056260
jbmitchell7 <jbmitchell747@gmail.com>
2021-11-16 14:14:15 -0600

    added code for scatterplot. not getting data because welcome screen is not redirecting locally

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row } from 'react-bootstrap';
+import {
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip
+} from 'recharts';
 
 import './nprogress.css';
 import './App.css';
@@ -63,7 +66,17 @@ class App extends Component {
     });
     const { currentLocation } = this.state;
     this.updateEvents(currentLocation, count);
-  }
+  };
+
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return { city, number };
+    })
+    return data;
+  };
 
   render() {
     if (this.state.showWelcomeScreen === undefined) return <div className="App" />
@@ -76,6 +89,18 @@ class App extends Component {
           </Row>
           <Row>
             <NumberOfEvents updateCount={this.updateCount} numberOfEvents={this.state.numberOfEvents} />
+          </Row>
+          <Row>
+            <ScatterChart width={400} height={400} margin={{
+              top: 20, right: 20, bottom: 20, left: 20,
+            }}
+            >
+              <CartesianGrid />
+              <XAxis type="category" dataKey="city" name="city" />
+              <YAxis type="number" dataKey="number" name="number of events" />
+              <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+              <Scatter data={this.getData()} fill="#8884d8" />
+            </ScatterChart>
           </Row>
           <Row className="event-list">
             <EventList events={this.state.events.slice(0, this.state.numberOfEvents)} />
